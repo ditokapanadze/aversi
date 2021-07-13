90f9309d60b667c2d08a0675bd38f87b2a6c47af
ditokapanadze <ditokapanadze0@gmail.com>
2021-07-13 14:19:30 -0700

    error handling

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
-require("dotenc").config({ path: "./config.env" });
+require("dotenv").config({ path: "./config.env" });
 const express = require("express");
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+
+connectDB();
+console.log(process.env.PORT);
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth"));
+
+// ეს ყოველთვის ბოლო უნდა იყოს
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`error: ${err}`);
+  server.close(() => process.exit(1));
+});
