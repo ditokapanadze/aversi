9f0ab4348451d615c1eb6b7eaee590689141623a
ditokapanadze <ditokapanadze111@gmail.com>
2021-08-10 02:13:20 -0700

    test

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello to  aversi API");
+});
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/product", require("./routes/product"));
 app.use("/api/private", require("./routes/private"));
