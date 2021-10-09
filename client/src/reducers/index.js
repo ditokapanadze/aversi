ae179611db56d2be78db81e6c3eb24a8df2b2701
ditokapanadze <ditokapanadze0@gmail.com>
2021-10-08 19:47:51 -0700

    test

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import product from "./product";
-import auth from "./auth";
-
-export default combineReducers({
-  product,
-  auth,
-});
--- client/src/screens/ForgotpasswordPage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ResetPass from "../components/ResetPass";
-
-function ForgotpasswordPage() {
-  return <ResetPass />;
-}
-
-export default ForgotpasswordPage;
